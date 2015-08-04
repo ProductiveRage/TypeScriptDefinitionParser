8955669005e2c507ec2f98e0846244ffbf1fd409
DanRoberts <DanRoberts@localhost>
2015-08-04 18:08:44 +0100

    Added the first tests

---
+++ TypeScriptDefinitionParserTests/IReadStringContent_Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using TypeScriptDefinitionParser.ContentReaders;
+
+namespace TypeScriptDefinitionParserTests.Parsers
+{
+    public static class IReadStringContent_Extensions
+    {
+        public static string ReadRemainingContent(this IReadStringContent reader)
+        {
+            if (reader == null)
+                throw new ArgumentNullException(nameof(reader));
+
+            var content = new StringBuilder();
+            while (reader.Current != null)
+            {
+                content.Append(reader.Current);
+                reader = reader.Next();
+            }
+            return content.ToString();
+        }
+    }
+}
